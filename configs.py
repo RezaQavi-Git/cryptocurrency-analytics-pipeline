853899a2c91a5f63b9aba0b9ef382feb9bef89fb
ReZa QaVi <reza.qavi@tapsi.cab>
2023-08-28 22:40:55 +0330

    Set more general configs

--- configs.py
+++ configs.py
@@ -4,18 +4,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-
-COINMARKETCAP_API_CONFIG = {
-    'url': 'https://pro-api.coinmarketcap.com/',
-    'headerAuthKey': 'X-CMC_PRO_API_KEY',
-    'token': os.environ.get('COINMARKETCAP_TOKEN'),
-    
+CMC_API_CONFIG = {
+    "url": "https://pro-api.coinmarketcap.com/",
+    "headerAuthKey": "X-CMC_PRO_API_KEY",
+    "token": os.environ.get("CMC_TOKEN"),
 }
+
+CMC_API_CONFIG_CRYPTO_LIST = ['ETH', 'BTC']
+
+CMC_TIMESTAMP_STR_PATTERN = "%Y-%m-%dT%H:%M:%S.%fZ"
 
 WALLEX_API_CONFIG = {
-    'url': 'https://api.wallex.ir/',
-    'headerAuthKey': 'X-API-Key',
-    'token': os.environ.get('WALLEX_TOKEN'),
-
+    "url": "https://api.wallex.ir/",
+    "headerAuthKey": "X-API-Key",
+    "token": os.environ.get("WALLEX_TOKEN"),
 }
 
+WALLEX_API_CONFIG_CRYPTO_LIST = ['USDTTMN']

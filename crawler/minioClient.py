80daf48cd5c28f2e4e3da11629d688f1b01d45f7
ReZa QaVi <reza.qavi@tapsi.cab>
2023-08-31 10:00:08 +0330

    Add write spark dataframe mechanism

--- crawler/minioClient.py
+++ crawler/minioClient.py
@@ -1,6 +1,6 @@
 from minio import Minio
 import json
-from io import BytesIO
+from io import BytesIO, BufferedReader
 
 from configs import MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY
 from utils import Logger
@@ -13,7 +13,7 @@ class MinIOClient:
                 endpoint=MINIO_ENDPOINT,
                 access_key=MINIO_ACCESS_KEY,
                 secret_key=MINIO_SECRET_KEY,
-                secure=False, 
+                secure=False,
             )
         except Exception as e:
             Logger().getLogger().error(f"Create MinIO Client Error: {e}")
@@ -25,7 +25,7 @@ class MinIOClient:
         if not self.minioClient.bucket_exists(bucket):
             self.minioClient.make_bucket(bucket)
 
-        return True        
+        return True
 
     def putObject(self, bucket: str, data: dict, folderName: str, fileName: str):
         data_json = json.dumps(data).encode("utf-8")
@@ -43,6 +43,27 @@ class MinIOClient:
             )
 
             Logger().getLogger().info(f"MinIO Upload Successful: {object_name}")
+
+        except Exception as e:
+            Logger().getLogger().error(f"MinIO Upload error: {e}")
+
+    def putDataframeObject(
+        self, bucket: str, jsonDataframe: str, folderName: str, fileName: str
+    ):
+        jsonArray = "[" + ",".join(jsonDataframe) + "]"
+        dataBytes = jsonArray.encode("utf-8")
+        dataStream = BufferedReader(BytesIO(dataBytes))
+
+        object_name = f"{folderName}/{fileName}.json"
+        try:
+            self.minioClient.put_object(
+                bucket_name=bucket,
+                object_name=object_name,
+                data=dataStream,
+                length=len(dataBytes),
+                content_type="application/json",
+            )
+            Logger().getLogger().info(f"MinIO Dataframe Upload Successful: {object_name}")
 
         except Exception as e:
             Logger().getLogger().error(f"MinIO Upload error: {e}")
